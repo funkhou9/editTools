6063031d50b271fc20ebedd255a0c02f453af5eb
Scott Funkhouser <sfunkhouser@gmail.com>
2015-07-16 11:06:46 -0400

    'roxygenized'

--- R/filters.R
+++ R/filters.R
@@ -7,11 +7,17 @@
 # @param n sample name or number
 # @param geno specify what genotype to filer - 0/0 1/1 or 0/1
 # @return filtered vcf object
-GTfilter <- function(obj, n, geno, remove=F) {
+#' @export
+gt_filter <- function(obj, n, geno, remove = FALSE) {
+  
+  # Get genotype
   GT <- samples(obj, n)[[1]]$GT
+  
+  # Remove or keep selected geno?
   if(remove){
     i <- !(as.character(GT) %in% geno)
   } else i <- as.character(GT) %in% geno
+  
   return(obj[i,])
 }
 
@@ -22,10 +28,15 @@ GTfilter <- function(obj, n, geno, remove=F) {
 #
 # @param obj vcf object
 # @return filtered vcf object
-GTdiffFilter <- function(obj) {
-  mat <- SampleMerge(obj, 2, 1)
-  i <- as.character(mat[,1]) != as.character(mat[,2])
-  return(obj[i,])
+#' @export
+gt_diff_filter <- function(obj) {
+  
+  # Get 'genotype' call for both DNA and RNA
+  mat <- sample_merge(obj, 2, 1)
+  
+  # Only return those variants 
+  i <- as.character(mat[, 1]) != as.character(mat[, 2])
+  return(obj[i, ])
 }
 
 
@@ -35,11 +46,15 @@ GTdiffFilter <- function(obj) {
 #
 # @param obj vcf object
 # @return filtered vcf object
-indelFilter <- function(obj) {
-  bases <- snps(obj)[,4:5]
-  i <- nchar(as.character(bases[,1])) == 1
-  j <- nchar(as.character(bases[,2])) == 1
-  return(obj[i+j==2,])
+#' @export
+indel_filter <- function(obj) {
+  
+  # Both REF and ALT columns need to be just 1 char in length
+  bases <- snps(obj)[, c("REF", "ALT")]
+  i <- nchar(as.character(bases[, 1])) == 1
+  j <- nchar(as.character(bases[, 2])) == 1
+  
+  return(obj[i & j, ])
 }
 
 
@@ -48,10 +63,12 @@ indelFilter <- function(obj) {
 # @param obj vcf object
 # @param qual quality threshold
 # @return filtered vcf object
-qualFilter <- function(obj, qual) {
-  #A simple quality filter that can be placed on the QUAL column
-  i <- snps(obj)[,6] > qual
-  return(obj[i,])
+#' @export
+qual_filter <- function(obj, qual) {
+  
+  # A simple quality filter that can be placed on the QUAL column
+  i <- snps(obj)[, 6] > qual
+  return(obj[i, ])
 }
 
 
@@ -60,18 +77,25 @@ qualFilter <- function(obj, qual) {
 # @param obj vcf object
 # @param depth the required depth of RNA in support of a mismatch
 # @return filtered vcf object
-editDepthFilter <- function(obj, depth) {
-  gt <- as.character(samples(obj, "DNA")[[1]][,"GT"])
-  #depedit - data.frame with DR DV for RNA
-  depedit <- samples(obj, "RNA")[[1]][,c("DP", "DV")]
+#' @export
+edit_depth_filter <- function(obj, depth) {
+  
+  
+  gt <- as.character(samples(obj, "DNA")[[1]][, "GT"])
+  
+  depedit <- samples(obj, "RNA")[[1]][, c("DP", "DV")]
   depedit <- sapply(depedit, function(x) as.numeric(as.character(x)))
-  depeditForIndexing <- cbind(DV=depedit[,"DV"], DR=depedit[,"DP"]-depedit[,"DV"])
-  #building matrix j to index depedit with
-  j <- sapply(gt, indexGeno)
+  depedit <- cbind(DV = depedit[, "DV"],
+                   DR = depedit[, "DP"] - depedit[,"DV"])
+  
+  # building matrix j to index depedit with
+  j <- sapply(gt, index_geno)
   j <- as.matrix(cbind(seq_along(j), j))
   rownames(j) <- NULL
   colnames(j) <- NULL
-  sampledepth <- depeditForIndexing[j]
+  
+  # index RNA calls based on DNA
+  sampledepth <- depedit[j]
   i <- sampledepth >= depth
   return(obj[i,])
 } 
@@ -79,27 +103,33 @@ editDepthFilter <- function(obj, depth) {
 
 # @param obj vcf object
 # @return filtered vcf object
-DPfilter <- function(obj, n, depth, depthv=F) {
+#' @export
+dp_filter <- function(obj, n, depth, depthv = FALSE) {
   #Minimum depth filter
   #At moment, must specify a single sample and depth filtering
   #   is performed on that sample.
   #By default, overall depth is the filtering criteria. If depthv=T, reads supporting variant become
   #   filtering criteria
   if(depthv) {
-    sampledepth <- as.numeric(as.character(samples(obj, n)[[1]][,"DV"]))
-  } else sampledepth <- as.numeric(as.character(samples(obj, n)[[1]][,"DP"]))
+    sampledepth <- as.numeric(as.character(samples(obj, n)[[1]][, "DV"]))
+  } else sampledepth <- as.numeric(as.character(samples(obj, n)[[1]][, "DP"]))
   i <- sampledepth >= depth
-  return(obj[i,])
+  return(obj[i, ])
 }
 
 
 # @param obj vcf object
 # @return filtered vcf object
-homFilter <- function(obj, n, perc=95) {
+#' @export
+hom_filter <- function(obj, n, perc = 95) {
   #Filters snps - a snp must be perc or 1-perc percent homozygous variant based on read counts
   #Must provide a single sample with <n> (order or name)
-  depths <- samples(obj, n)[[1]][,c("DP","DV")]
-  pervar <- as.numeric(as.character(depths[,"DV"]))/as.numeric(as.character(depths[,"DP"]))*100
-  i <- pervar >= perc | pervar <= (100-perc)
-  return(obj[i,])
+  depths <- samples(obj, n)[[1]][, c("DP", "DV")]
+  pervar <- as.numeric(as.character(depths[, "DV"])) /
+              as.numeric(as.character(depths[,"DP"])) * 100
+  i <- pervar >= perc | pervar <= (100 - perc)
+  return(obj[i, ])
 }
+
+
+
