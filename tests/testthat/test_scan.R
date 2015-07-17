41bf9da1438ff0ea02f19070da4d0b6ea39e34db
Scott Funkhouser <sfunkhouser@gmail.com>
2015-07-17 12:00:31 -0400

    usable tests

--- tests/testthat/test_scan.R
+++ tests/testthat/test_scan.R
@@ -1,5 +1,5 @@
 library(editTools)
-context("Test the scanning for edits - one step at a time")
+context("Test RNA edit search functionality")
 
 LD_plus <- read_vcf("LD_plus_sample.vcf.gz", 2, c("DNA", "RNA"))
 LD_minus <- read_vcf("LD_minus_sample.vcf.gz", 2, c("DNA", "RNA"))
@@ -8,13 +8,23 @@ liver_minus <- read_vcf("liver_minus_sample.vcf.gz", 2, c("DNA", "RNA"))
 fat_plus <- read_vcf("fat_plus_sample.vcf.gz", 2, c("DNA", "RNA"))
 fat_minus <- read_vcf("fat_minus_sample.vcf.gz", 2, c("DNA", "RNA"))
 
+test_that("Edit summary finds expected sites", {
+  expect_equal(edit_summary(liver_plus)$Calls[, "POS"], c(109593,
+                                                          110920,
+                                                          114093,
+                                                          154141))
+  expect_equal(edit_summary(liver_plus)$Calls[, "CHROM"], c(10,
+                                                            10,
+                                                            10,
+                                                            10))
+  
+  expect_equal(edit_summary(fat_plus)$Calls[, "POS"], c(114093,
+                                                        163291))
+  expect_equal(edit_summary(fat_plus)$Calls[, "CHROM"], c(10,
+                                                          10))
+})
 
 
 
-liver_plus_edits <- edit_summary(liver_plus)
-
-fat_plus_edits <- edit_summary(fat_plus)
-fat_minus_edits <- edit_summary(fat_minus)
 
 
-edit_summary(fat_plus)
