18cb48718ce07d03d0c600ae29c8922985ab2f3c
Scott Funkhouser <sfunkhouser@gmail.com>
2015-08-04 22:49:29 -0400

    initial commit

---
+++ R/add_mirna.R
@@ -0,0 +1,35 @@
+#' Add miRNA target data to an edit_table object
+#'
+#' @param this a edit_table object
+#' @param mirna_file character string naming formatted miranda ouptut file
+#' @return a newly formatted edit_table object with miRNA target information
+#' @export
+add_mirna <- function(this, mirna_file) {
+  
+  mod_result <-
+    capture.output(mbym_search(as.matrix(this$AllSites), mirna_file, 2, 3, 4, 3, 4, 0, 1, TRUE, 5)) %>%
+      sapply(function(x) strsplit(x, split = '\t')) %>%
+        do.call(rbind, .) %>%
+          as.data.frame(stringsAsFactors = FALSE)
+  
+  # Format output - replace sequence depth data with repeatmasker data
+  mod_result <- mod_result[, c(1, 2, 3, 4, 10, 11, 12, 13, 9)]
+  rownames(mod_result) <- NULL  
+  colnames(mod_result) <- c("Chr",
+                            "Pos",
+                            "Strand",
+                            "Mismatch",
+                            "Target_start",
+                            "Target_end",
+                            "miRNA",
+                            "Target_gene",
+                            "Tissue")
+  
+  new_result <- append(this,
+                       list("mirnaTargetSites" = mod_result),
+                       1)
+  
+  class(new_result) <- "edit_table"
+  
+  return (new_result)
+}
