7991df13d14705f93f19d7f504e514da824b2de7
Scott Funkhouser <sfunkhouser@gmail.com>
2015-08-06 17:34:44 -0400

    much improved usability and features

--- R/plot.edit_table.R
+++ R/plot.edit_table.R
@@ -1,37 +1,25 @@
-#' A plotting tool to visualize DNA RNA mismatch counts from an edit_frame
+#' A plotting tool to visualize data from an edit_frame object
 #' 
 #' @param this an edit_table object
-#' @param character naming member of edit_table to plot. A barplot is produced
+#' @param field character naming member of edit_table to plot. A barplot is produced
 #'  for members $AllSites, $RepSites, $mirnaTargetSites, and $VEP. A Venn Diagram
 #'  is produced for member $Tissues
+#' @param event character naming which column from field to count
+#' @param mismatch character indicating which type of mismatch to consider, such as "AtoG".
+#'  By default, considers all mismatch types
+#' @param n integer limiting number of events to n. Any number of events beyond
+#'  n will be grouped into "other" category
 #' @import ggplot2
 #' @import grid
 #' @import VennDiagram
 #' @export
-plot.edit_table <- function(this, field = "AllSites") {
+plot.edit_table <- function(this,
+                            field = "AllSites",
+                            event = "Mismatch",
+                            mismatch = "all",
+                            n = NULL) {
   
   member <- this[[field]]
-  
-  # Names of each tissue in this object. Sorted from lowest edits to highest
-  tiss_names <- table(member$Tissue) %>%
-    sort() %>%
-      names()
-  
-  # Each type of mismatch present in the dataset
-  mismatch_names <- unique(member[, "Mismatch" ]) 
-  
-  # Total num of mimsatch
-  tot_cnt <- nrow(member)
-  
-  # Mismatch types present in each tissue
-  types <- sapply(tiss_names,
-                  function(x) {
-                    member[member$Tissue == x, "Mismatch"] %>%
-                      unique()
-                  })
-  
-  # Number of mismatch types in each tissue
-  type_counts <- sapply(types, length)
   
 
   if (field == "Tissues") {
@@ -60,42 +48,62 @@ plot.edit_table <- function(this, field = "AllSites") {
     
   } else {
     
-    # Obtain each tissue summary
-    mismatches <- lapply(tiss_names,
-                         count_mismatch,
-                         this[[field]],
-                         wname = TRUE)
+    # Obtain each tissue summary with added args to count_match()
+    event_dat <- count_mismatch(member,
+                                wname = TRUE,
+                                event = event,
+                                trim = n,
+                                mismatch = mismatch)
     
-    mismatches <- do.call(rbind, mismatches)
-    names(mismatches)[3] <- "Tissue"
- 
+    
+    
+    # Join list and ensure proper formatting needed for plotting
+    event_dat <- do.call(rbind, event_dat)
+    event_dat[, c("Freq")] <- as.numeric(event_dat[, c("Freq")])
+    event_dat[, c("Prop")] <- as.numeric(event_dat[, c("Prop")])
+    colnames(event_dat)[1] <- "Event"
+  
+    
+    # Names of each tissue in this object. Sorted from lowest edits to highest
+    tiss_names <- unique(event_dat$Tissue)
+    
+    # Each type of mismatch present in the dataset
+    event_names <- unique(event_dat$Event) 
+    
+    # Total num of events in all of data field
+    tot_cnt <- nrow(member)
+    
+
     # Obtain total proportions of each edit across each tissue,
     #   these are used as labels within the plot
     total_prop <- 
-      sapply(mismatch_names,
+      sapply(event_names,
              function(x) {
-              cnt <- mismatches[mismatches$Mismatch == x, "Freq"]
-              round (sum(cnt / tot_cnt) * 100, 2) %>%
-                paste('%', sep = '')
-           })
+              cnt <- event_dat[event_dat$Event == x, "Freq"]
+              cnt_perc <- round (sum(cnt / tot_cnt) * 100, 2) %>%
+                            paste('%', sep = '')
+              return (cnt_perc)
+             })
+    
     
     # Obtain index for each mismatch type - where is it represented last
-    #   in the mismatches object? (Necessary for adding total_prop to mismatches)
+    #   in the event_dat object? (Necessary for adding total_prop to event_dat)
     type_idx <- 
-      sapply(mismatch_names,
-            function(x) {
-              which(mismatches$Mismatch == x) %>%
+      sapply(event_names,
+             function(x) {
+              which(event_dat$Event == x) %>%
                 max()
-           })
+            })
     
-    # Add total_prop to each row of mismatches, appropriately
-    mismatches[type_idx, "Total_prop"] <- total_prop
-    
-    
-    ggplot(mismatches,
-           aes(x = reorder(Mismatch, -Freq),
+    # Add total_prop to each row of event_dat, appropriately
+    event_dat[type_idx, "Total_prop"] <- total_prop
+  
+
+  
+    ggplot(event_dat,
+           aes(x = reorder(Event, -Freq),
                y = Freq,
-               fill = Tissue)) +
+               fill = reorder(Tissue, Freq))) +
       geom_bar(stat = 'identity') +
       geom_text(aes(label = Total_prop),
                 na.rm = TRUE,
