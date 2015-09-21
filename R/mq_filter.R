86c4df73be4038bd5c582d4011b349f117b78bba
Scott Funkhouser <sfunkhouser@gmail.com>
2015-09-21 14:57:06 -0400

    mq_filter - for filtering candidates based on conditional probability of obtaining "edited" allele

---
+++ R/mq_filter.R
@@ -0,0 +1,21 @@
+#' Filters potential RNA editing sites by applying filtering on average mapping quality (MQ)
+#' 
+#' Calculates P(X >= x | N, p(e)) where x is the number of reads which provide support for
+#' editing for sample i at site j, N is the total depth for sample i at site j,
+#' and p(e) is the probability of error for site j, derived from the average mapping quality.
+#'
+#' @param this an edit_table object 
+#' @export
+mq_filter <- function(this) {
+  
+  # Obtained average quality for each site and convert to probability
+  ave_mq <- as.numeric(this$AllSites$Ave_MQ)
+  pe <- 10^-(ave_mq / 10)
+  
+  # Total rna depths and mismatch depths
+  rna_dp <- as.numeric(this$AllSites$RNA_Depth)
+  edit_dp <- as.numeric(this$AllSites$Mismatch_Depth)
+  
+  # Estimate conditional probability P(X >= x | N, p(e)) using binomial model
+  1 - pbinom(edit_dp, rna_dp, pe)
+}
